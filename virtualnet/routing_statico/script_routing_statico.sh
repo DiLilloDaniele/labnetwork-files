51c15c953f5c3d199ae9d7cce06260d49e3b5fe2
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-03-19 19:38:48 +0100

    Added address and default gateways

--- virtualnet/routing_statico/script_routing_statico.sh
+++ virtualnet/routing_statico/script_routing_statico.sh
@@ -28,3 +28,33 @@ ip link set v-s2 netns r2
 ip netns exec r2 ip link set v-s2 up
 ip link set v-s3 netns r3
 ip netns exec r3 ip link set v-s3 up
+
+ip netns exec h1 ip addr add 192.168.1.1/24 dev veth0
+ip netns exec r1 ip addr add 192.168.1.253/24 dev veth0
+#ip netns exec r1 ip addr add 192.168.1.253/24 dev veth1
+
+ip netns exec h2 ip addr add 192.168.2.1/24 dev veth0
+
+ip netns exec r2 ip addr add 192.168.2.253/24 dev veth0
+ip netns exec r2 ip addr add 10.0.0.1/30 dev v-s2
+
+ip netns exec r3 ip addr add 10.0.0.2/30 dev v-s3
+ip netns exec r3 ip addr add 192.168.3.253/24 dev veth0
+ip netns exec h3 ip addr add 192.168.3.1/24 dev veth0
+
+ip netns exec h1 route add default gw 192.168.1.253
+ip netns exec h2 route add default gw 192.168.2.253
+ip netns exec h3 route add default gw 192.168.3.253
+
+ip netns exec r1 sysctl -w net.ipv4.ip_forward=1
+ip netns exec r2 sysctl -w net.ipv4.ip_forward=1
+ip netns exec r3 sysctl -w net.ipv4.ip_forward=1
+
+ip netns exec r1 ip route add 192.168.3.0/24 via 192.168.2.253 dev veth1 onlink
+ip netns exec r1 ip route add 192.168.2.0/24 via 192.168.2.253 dev veth1 onlink
+
+ip netns exec r2 ip route add 192.168.1.0/24 via 192.168.1.253 dev veth0 onlink
+ip netns exec r2 ip route add 192.168.3.0/24 via 10.0.0.2
+
+ip netns exec r3 ip route add 192.168.2.0/24 via 10.0.0.1
+ip netns exec r3 ip route add 192.168.1.0/24 via 10.0.0.1
